ae48b8f89fc3916b15f29e1d26f87d53d33c3261
zackcglee <leechanggong@gmail.com>
2022-11-21 13:36:26 +0900

    minor enhancement with status message on panel

--- server.py
+++ server.py
@@ -9,19 +9,36 @@ from gensim.models.doc2vec import Doc2Vec
 from scipy import spatial
 from nltk.tokenize import word_tokenize
 import nltk
+print(' ___  ____  __  __  ____  ___    ____  _____  _____  __   \n' +
+        '/ __)(_  _)(  \/  )(_  _)/ __)  (  _ \(  _  )(  _  )(  )  \n' +
+    '\__ \ _)(_  )    (  _)(_( (__    )___/ )(_)(  )(_)(  )(__ \n' +
+    '(___/(____)(_/\/\_)(____)\___)  (__)  (_____)(_____)(____)\n')
+print("\nChecking for necessary packages....")
 nltk.download('punkt')
 
 suggestions = []
 similarity_candidate = []
+match_num = 0
+match_cnt = 0
+mutex = 0
 
-def detreefy_builder(static, change_vector:str, snap:str):
+'''
+This function creates threads for each exact matches in the pool.
+@:param static: the static binary tree of data structure(pool), change vector:target editscript to look for, snap: the snapshot of the code
+'''
+def detreefy_builder(static:dt, change_vector:str, snap:str):
     global similarity_candidate
     global suggestions
+    global match_num
+    global match_cnt
     sugg, match_num = dt.detreefy(static, change_vector)
+    match_cnt = 0
     snap_vec = d2v_model.infer_vector(tokenize(snap))
     if sugg is None:
         # TO DO: heuristics for finding similar change vectors
         # child -> sibling -> parent
+        print('There is no exact match in the pool')
+        match_cnt = -1
         sys.exit()
     else:
         idx:int = 0
@@ -29,6 +46,7 @@ def detreefy_builder(static, change_vector:str, snap:str):
         print(str(match_num) + ' exact matches found' + '\n')
         for i in range(0, match_num):
             suggestion = sugg[i]
+            match_cnt = i
             builder.append(threading.Thread(target=suggestions_builder, args=(suggestion[0], suggestion[2], suggestion[3], suggestion[4])))
             print(idx, len(builder))
             builder[idx].start()
@@ -38,25 +56,27 @@ def detreefy_builder(static, change_vector:str, snap:str):
                     builder[j].join()
                 idx = 0
                 builder = []
-                # for s in similarity_candidate:
-                #     print(s)
-                #     print('=====================================================')
                 if len(similarity_candidate) >= 10:
                     sim = [cos_sim(s, snap_vec) for s in similarity_candidate]
                     val, index = min((val, index) for (index, val) in enumerate(sim))
                     suggestions.append(similarity_candidate[index])
                     print(len(suggestions),'\n',similarity_candidate[index])
                     similarity_candidate = []
+    match_cnt = -1
         
                         
                     
             
-def cos_sim(diff,snap_vec):
+def cos_sim(diff:str,snap_vec):
     vec = d2v_model.infer_vector(tokenize(diff))
     cos_distance = spatial.distance.cosine(vec, snap_vec)
     return cos_distance
 
-def tokenize (code):
+'''
+@:param code: a source code in string
+@:return: flat:a list of tokens that can be used for embeddings
+'''
+def tokenize (code:str):
     lines = code.split('\n')[7:]
     code = ''
     for line in lines:
@@ -71,7 +91,7 @@ def tokenize (code):
     flat = [item for l in non_flat for item in l if item != '']
     return flat
             
-def suggestions_builder(proj, pc, file, line):
+def suggestions_builder(proj:str, pc:str, file:str, line:str):
     global similarity_candidate
     process = subprocess.Popen(['make', 'pp_run','proj='+proj, 'pc='+pc, 'file='+file, 'line='+line], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     code = ''
@@ -92,6 +112,7 @@ def suggestions_builder(proj, pc, file, line):
     sys.exit()
 
 def processMessages(conn, addr, static):
+    global match_cnt
     while True:
         try:
             data = conn.recv(5000)
@@ -101,7 +122,12 @@ def processMessages(conn, addr, static):
             data = data.decode('utf-8')
             if data == 'GET':
                 if len(suggestions) == 0:
-                    conn.sendall(bytes('It is either we don\'t have enough snapshots or it is still processing. \nPlease try later...','utf-8'))
+                    if len(similarity_candidate) == 0:
+                        conn.sendall(bytes('We do not have enough snapshots\nPlease try taking more snapshots...','utf-8'))
+                    elif match_cnt == -1:
+                        conn.sendall(bytes('We could not find any relavant changes..\n Try with other snapshots...','utf-8'))
+                    else:
+                        conn.sendall(bytes(f'We are looking for semantically relavant change...\n We are processing {match_cnt}th file out of {match_num}matches\nTry in a bit.','utf-8'))
                 else:
                     conn.sendall(bytes(suggestions[0], 'utf-8'))
                     suggestions.pop(0)
@@ -109,13 +135,13 @@ def processMessages(conn, addr, static):
                 tmp = data.split('!@#$%')
                 change_vector = tmp[0]
                 snap = tmp[1]
-                print(change_vector)
+                print(f'Recieved Change Vector: {change_vector}')
                 conn.sendall(bytes('Thank you for connecting', 'utf-8'))
                 detreefier = threading.Thread(target=detreefy_builder, args=(static, str(change_vector),snap))
                 detreefier.start()
         except:
             conn.close()
-            print("Connection closed by", addr)
+            # print("Connection closed by", addr)
             # Quit the thread.
             sys.exit()
 
@@ -123,7 +149,7 @@ def main():
         while True:
             # Wait for connections
             conn, addr = s.accept()
-            print('Got connection from ', addr[0], '(', addr[1], ')')
+            # print('Got connection from ', addr[0], '(', addr[1], ')')
             # Listen for messages on this connection
             listener = threading.Thread(target=processMessages, args=(conn, addr, static))
             listener.start()
@@ -133,22 +159,30 @@ def main():
 if len(sys.argv) != 3:
     print('Usage: python3 server.py <IP> <port>')
     sys.exit()
-s = socket.socket()                      # Create a socket object
-host = sys.argv[1]
-#"127.0.0.1"            # Get local machine name
 
-port = sys.argv[2]
-#64555                             # Reserve a port for your service.
+s = socket.socket()                             # Create a socket object
+host = sys.argv[1]                              # Get local machine name
+#"127.0.0.1"            
+
+port = int(sys.argv[2])                              # Reserve a port for your service.
+#64555                             
 s = socket.socket()
-s.bind((host, int(port)))                     # Bind to the port
 
-s.listen(5)                              # Now wait for client connection.
+try:
+    print('\nChecking the port number and host IP address...')
+    s.bind((host, port))                       # Bind to the port
+except:
+    print('Bind failed. Error : ' + str(sys.exc_info()))
+    sys.exit()
 
-d2v_model = Doc2Vec.load('./train/d2v.model')
+s.listen(5)                                     # Now wait for client connection.
 
-with open('./pool.tree','rb') as file:
-        unpickler = pickle.Unpickler(file)
+d2v_model = Doc2Vec.load('./train/d2v.model')   # load the Doc2Vec model
+print("\nLoading the pool...")
+with open('./pool.tree','rb') as file:          # load the pool
+        unpickler = pickle.Unpickler(file)      
         print(unpickler.__str__)
         static = unpickler.load()
         if __name__ == '__main__':
+            print('\n\nReady for service!')
             main()
